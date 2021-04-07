13bad0e90923bdaf73734e717751397eb7bd9f7d
nguyendacchien <nguyendacchien02032002@gmail.com>
2021-04-07 15:40:22 +0700

    jhf

---
+++ chuvi.js
@@ -0,0 +1,5 @@
+let r;
+r = +prompt("bán kính r")
+const PI=3.14;
+let C =2*PI*r;
+document.write("chu vi hình tròn C=" + C);
---
+++ dientichHC.js
@@ -0,0 +1,5 @@
+let inputr;
+inputr = +prompt("bán kính r")
+const PI=3.14;
+let S = PI*(inputr*inputr);
+document.write("diện tích hình tròn S=" + S);
